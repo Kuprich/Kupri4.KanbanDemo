71b8171575d13f6056aa77e7140de5fa47885eaf
ilya Kupriyanov <ilya-kyp@yandex.ru>
2022-08-04 20:44:24 +0300

    подредактивана страница index.razor. Добавлены данные о перемещаемой карте и данные о стоблце, в который она перемещается.  (в целом, можно передавать только id столбцов)

--- Kupri4.KanbanDemo/wwwroot/js/site.js
+++ Kupri4.KanbanDemo/wwwroot/js/site.js
@@ -1,19 +1,32 @@
 ﻿function onBlazorReady() {
 
-    Sortable.create(column_0, {
-        group: "shared",
-        animation: 150,
-        ghostClass: 'blue-background-class'
-    });
-    Sortable.create(column_1, {
-        group: "shared",
-        animation: 150,
-        ghostClass: 'blue-background-class'
-    });
-    Sortable.create(column_2, {
-        group: "shared",
-        animation: 150,
-        ghostClass: 'blue-background-class'
-    });
+    let colums = [column_0, column_1, column_2]
+
+    for (var i = 0; i < colums.length; ++i) {
+       
+        Sortable.create(colums[i], {
+            group: "shared",
+            animation: 150,
+            ghostClass: 'blue-background-class',
+        })
+    }
+    
+    //Sortable.create(column_0, {
+    //    group: "shared",
+    //    animation: 150,
+    //    ghostClass: 'blue-background-class'
+    //});
+
+    //Sortable.create(column_1, {
+    //    group: "shared",
+    //    animation: 150,
+    //    ghostClass: 'blue-background-class'
+    //});
+
+    //Sortable.create(column_2, {
+    //    group: "shared",
+    //    animation: 150,
+    //    ghostClass: 'blue-background-class'
+    //});
 
 }
